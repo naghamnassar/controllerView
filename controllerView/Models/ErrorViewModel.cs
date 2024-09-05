970e352445c80f48d60e49887716c566254db109
iQraa <iQraa@DESKTOP-JJUA377>
2024-09-06 00:51:21 +0300

    Add project files.

---
+++ controllerView/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace controllerView.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
